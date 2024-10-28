cbb36e9aba1347fc80a12e5d762da84f749588cf
Gleb <gklejkov@gmail.com>
2024-10-29 01:57:39 +0700

    переделана задача 2

--- task7.py
+++ task7.py
@@ -4,9 +4,9 @@
 # со сторонами side1, side2, side3, или False в противном случае.
 def is_valid_triangle(side1, side2, side3):
     if (side1+side2)>side3 and (side1+side3)>side2 and (side3+side2)>side1:
-        print("True")
+        return True
     else:
-        print("False")
+        return False
 side1=int(input())
 side2=int(input())
 side3=int(input())
