02f425addc223e97cfe551cedd4f50b8729405e2
Gleb <gklejkov@gmail.com>
2024-10-28 09:37:58 +0700

    решена задача 6

--- task6.py
+++ task6.py
@@ -8,4 +8,16 @@
 # 25 15
 # Вывод:
 # 5 3
-
+mas1=[]
+mas2=[]
+def f(n,k):
+    for i in range(1,min(n,k)+1):
+        if min(n,k)%i==0:
+            mas1.append(i)    
+    for j in mas1:
+        if max(n,k)%j==0:
+               mas2.append(j)
+    print(n//max(mas2),k//max(mas2))
+n=int(input())
+k=int(input())
+f(n,k)
