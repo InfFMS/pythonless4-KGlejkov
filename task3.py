27cf55eff962bf4ac1061c1720c03a22b5c3d0e0
Gleb <gklejkov@gmail.com>
2024-10-28 09:30:33 +0700

    решена задача 3

--- task3.py
+++ task3.py
@@ -5,4 +5,28 @@
 # Пример:
 # 2013
 # MMXIII
+def f(n):
+    val = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+    ]
 
+    a = ''
+
+    for (integer, f) in val:
+        while n >= integer:
+            a += f
+            n -= integer
+
+    return a
