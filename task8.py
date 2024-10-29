7d53d55621a9360c65bdcba778c6539fb84a6cf2
Gleb <gklejkov@gmail.com>
2024-10-29 21:34:27 +0700

    решена задача 8 через рекурсию

--- task8.py
+++ task8.py
@@ -6,12 +6,15 @@
 # 378
 # Вывод:
 # 2*3*3*3*7
-def f(x):
-    for i in range(2,int(x**0.5)+1):
-        while x%i==0:
-            print(i)
-            x//=i
-    if x!=1:
-        print(x)
-x=int(input())
-f(x)
+def f(n):
+    if n==1:
+        return " "
+    else:
+        for i in range(2, int(n**0.5)+2):
+            if n%i==0:
+                n//=i
+                return str(i)+"*"+str(f(n))
+                break
+n=int(input())
+print(f(n)[:-2])
+
